dda2146cfbfbd8a7a985ff09411aaca1b6c02e79
Shanda Shaw <srh0029@hotmail.com>
2024-07-16 21:02:01 -0500

    styled MyActivity EditReviewForm component, Nav bar, & cart icon

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom"
+import './Nav.css'
 
 const Nav = () => {
     return (
         <div className="nav">
             <Link to="/">
-                <h2>Home</h2>
+                <h2 className="navLink">Home</h2>
             </Link>
             <Link to="/about">
-                <h2>About Us</h2>
+                <h2 className="navLink">About Us</h2>
             </Link>
             <Link to="/pieces">
-                <h2>Shop</h2>
+                <h2 className="navLink">Shop</h2>
             </Link>
             <Link to={`/myactivity`}>
-                <h2>My Activity</h2>
+                <h2 className="navLink">My Activity</h2>
             </Link>
         </div>
     )
